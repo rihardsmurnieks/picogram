9985efbf82f25eaa5a82963a47a74b0ac4e979d7
rihardsmurnieks <rihardsmurnieks12@gmail.com>
2017-07-19 16:54:52 +0300

    adding initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+
+'use strict';
+//import './node_modules/font-awesome/css/font-awesome.css';
+import './style.less';
+
+let div=document.getElementById('picogram');
+console.log(typeof div);
+div.innerHTML = 'picogram';
